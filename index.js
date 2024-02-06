dc0081b87e3986542d6bb333824f99f905f10b8b
Harry CHOI <h2choi@ucsd.edu>
2024-02-06 15:55:25 -0800

    started integrating expressjs and pgadmin

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import 'dotenv/config';
+import express from 'express';
+import { pool } from './src/data/postgresDB.js';
+import userRouter from './src/controller/users.js';
+
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hi everyone')
-})
+});
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
+app.use('/users', userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+});
---
+++ src/controller/users.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { pool } from '../data/postgresDB.js';
+const userRouter = express.Router();
+
+
+/*
+* db connection test api
+*/
+userRouter.get('/test-db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const { rows } = await client.query('SELECT NOW()'); // This is a simple query to test the connection
+        res.json({ currentTime: rows[0].now }); // Send back the current time as a response
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database connection error');
+    }
+});
+
+
+
+
+export default userRouter;
