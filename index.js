796bf6cdb103b02c6dd2e8335bb12178d0da520f
Harry CHOI <h2choi@ucsd.edu>
2024-02-08 02:30:23 -0800

    added submit api

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import express from 'express';
 import { pool } from './src/data/postgresDB.js';
 import userRouter from './src/controller/users.js';
+import recordRouter from './src/controller/records.js';
 
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for html form post request
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', userRouter);
+app.use('/record', recordRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
